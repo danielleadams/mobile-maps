d80ba82000695ff8472b616041bc4b573ac8fa02
Danielle Adams <danielle.adams@heroku.com>
2019-10-11 03:14:41 -0400

    fix app

--- index.js
+++ index.js
@@ -1,8 +1,8 @@
 const express = require('express');
 const path = require("path");
 
+const app = express();
 const port = 3000;
 
-express()
-  .use(express.static(path.join(__dirname, "/public")))
-  .listen(port, () => console.log(`Listening on port ${port}!`));
+app.use(express.static(path.join(__dirname, "/public")));
+app.listen(port, () => console.log(`Listening on port ${port}!`));
